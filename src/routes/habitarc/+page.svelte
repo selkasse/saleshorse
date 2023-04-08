50da474a5a38d4f68ed9f67aa3cf374b81320293
Sharif Elkassed <selkasse@gmu.edu>
2023-04-07 21:36:51 -0400

    update image url for twitter card

--- src/routes/habitarc/+page.svelte
+++ src/routes/habitarc/+page.svelte
@@ -10,7 +10,7 @@
 		name="twitter:description"
 		content="A habit archive; a place to see the arc of your habit's data over time"
 	/>
-	<meta name="twitter:image" content="https://imgur.com/vnae1m7" />
+	<meta name="twitter:image" content="https://i.imgur.com/vnae1m7.png" />
 </svelte:head>
 
 <div class="text-column">
