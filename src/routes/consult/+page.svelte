01d5cbd679e8007decdf561ac732b9a3ed09c9f1
Sharif Elkassed <selkasse@gmu.edu>
2023-04-09 00:28:03 -0400

    add consult redirect page

---
+++ src/routes/consult/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>thank you!</title>
+	<meta name="description" content="Consult thank-you page" />
+</svelte:head>
+
+<section>
+	<div class="text-column">
+		<h1>looking forward to speaking with you, <span>name goes here!</span></h1>
+	</div>
+</section>
+
+<style>
+	span {
+		color: var(--color-theme-1);
+	}
+</style>
