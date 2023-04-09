97c6b51eb5d596df39eaab33ce370bb7f50b86e2
Sharif Elkassed <selkasse@gmu.edu>
2023-04-09 18:25:10 -0400

    log url params

--- src/routes/consult/+page.svelte
+++ src/routes/consult/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+	let queryString = window.location.search;
+	let urlParams = new URLSearchParams(queryString);
+	let email = urlParams.get('invitee_email');
+	let name = urlParams.get('invitee_full_name');
+	console.log(`email: ${email}`);
+	console.log(`name: ${name}`);
+</script>
+
 <svelte:head>
 	<title>thank you!</title>
 	<meta name="description" content="Consult thank-you page" />
