84b27e4cab2fea037606b61e2d64e6e4ab928888
Sharif Elkassed <selkasse@gmu.edu>
2023-04-11 00:38:30 -0400

    add consult thank-you route

--- src/routes/consult/+page.svelte
+++ src/routes/consult/+page.svelte
@@ -1,58 +1,67 @@
 <script>
-	// import { browser } from '$app/environment';
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 
-	// let queryString = window.location.search;
-	// let urlParams = new URLSearchParams(queryString);
-	// let email = urlParams.get('invitee_email');
-	// let name = urlParams.get('invitee_full_name');
+	const url = $page.url;
 
-	// ;
+	let email = url.searchParams.get('invitee_email');
+	let name = url.searchParams.get('invitee_full_name');
+	let firstName = name?.substring(0, name.indexOf(' '));
+	let lastName = name?.substring(name.indexOf(' ') + 1);
 
-	onMount(async () => {
-		const url = $page.url;
+	let BASE_URL = `https://b5ae-2600-4040-4751-3e00-f037-6e12-306f-5051.ngrok-free.app`;
 
-		console.log(`email: ${url.searchParams.get('invitee_email')}`);
-		console.log(`name: ${url.searchParams.get('invitee_full_name')}`);
-	});
-	// if (browser) {
-	// }
-</script>
+	let returnURL = `${BASE_URL}/consult/thank-you?firstName=${firstName}`;
 
-<!-- <script context="module">
-	// @ts-ignore
-	export function load({ url }) {
-		const email = url.searchParams.get('invitee_email') || '/consult';
-		const name = url.searchParams.get('invitee_full_name') || '/consult';
-		return {
-			props: {
-				email,
-				name
-			}
-		};
+	/**
+	 * @type {HTMLInputElement}
+	 */
+	export let submitButton;
+
+	$: {
+		clickButton(submitButton);
+	}
+
+	/**
+	 * @param {HTMLInputElement} button
+	 */
+	function clickButton(button) {
+		if (button) {
+			button.click();
+		}
 	}
 </script>
 
-<script>
-	export let email;
-	export let name;
-	console.log(email);
-	console.log(name);
-
-	// do stuff
-</script> -->
-
 <svelte:head>
 	<title>thank you!</title>
-	<meta name="description" content="Consult thank-you page" />
+	<meta name="description" content="Consult redirect page" />
 </svelte:head>
 
 <section>
 	<div class="text-column">
-		<h1><span>looking forward</span> to speaking with you!</h1>
+		<h1>looking forward to speaking with you, <span>{firstName}!</span></h1>
 	</div>
 </section>
+
+<form
+	action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8"
+	method="POST"
+	class="form"
+>
+	<input type="hidden" name="oid" value="00D8Y000001Qcn8" />
+	<input type="hidden" name="retURL" value={returnURL} />
+	<input type="hidden" name="ProductInterest__c" value="CONS" />
+	<input type="hidden" name="first_name" value={firstName} />
+	<input type="hidden" name="last_name" value={lastName} />
+	<input type="hidden" name="email" value={email} />
+
+	<input
+		bind:this={submitButton}
+		type="submit"
+		name="submit"
+		style="display: none"
+		value="submit"
+	/>
+</form>
 
 <style>
 	span {
---
+++ src/routes/consult/thank-you/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+
+	const url = $page.url;
+
+	let firstName = url.searchParams.get('firstName');
+</script>
+
+<section>
+	<div class="text-column">
+		<h1>looking forward to speaking with you, <span>{firstName}!</span></h1>
+	</div>
+</section>
+
+<style>
+	span {
+		color: var(--color-theme-1);
+	}
+</style>
